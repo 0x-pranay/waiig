dac3d79fb443d0b94e317310b5724340cbd3d288
Pranay Velisoju <velisojupranay@gmail.com>
2023-06-25 00:36:14 +0530

    test: return value of String() method in Node interface

--- monkey/ast/ast.go
+++ monkey/ast/ast.go
@@ -47,7 +47,7 @@ func (p *Program) TokenLiteral() string {
 func (p *Program) String() string {
   var out bytes.Buffer
 
-  for _, s := range p.Statments {
+  for _, s := range p.Statements {
     out.WriteString(s.String())
   }
   return out.String()
